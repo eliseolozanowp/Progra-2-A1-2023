0840b4f0148ff8e0c37e4914ad28a8e053288d09
elise <USIS034221@ugb.edu.sv>
2023-02-19 21:56:31 -0600

    Conversores

--- app/src/main/java/com/ugb/progra2_a1/MainActivity.java
+++ app/src/main/java/com/ugb/progra2_a1/MainActivity.java
@@ -11,10 +11,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TabHost tbh;
-    TextView temp;
-    Spinner spn;
     Button btn;
-    conversores miConversor= new conversores();
+    TextView temp;
+    Spinner spnDe, spnA;
+    conversores miConversor = new conversores();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,49 +24,53 @@ public class MainActivity extends AppCompatActivity {
         tbh = findViewById(R.id.tbhConversores);
         tbh.setup();
 
-        tbh.addTab(tbh.newTabSpec("longitud").setContent(R.id.tbLongitud).setIndicator("", getDrawable(R.drawable.longitud)));
-        tbh.addTab(tbh.newTabSpec("monedas").setContent(R.id.tbMonedas).setIndicator("", getDrawable(R.drawable.money)));
-        tbh.addTab(tbh.newTabSpec("masa").setContent(R.id.tbMasa).setIndicator("", getDrawable(R.drawable.masa)));
+        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tbhLongitud).setIndicator("", getDrawable(R.drawable.length_measure_icon)));
+        tbh.addTab(tbh.newTabSpec("Almacenamiento").setContent(R.id.tbhAlmcenamiento).setIndicator("", getDrawable(R.drawable.almacenamiento)));
+        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tbhMonedas).setIndicator("", getDrawable(R.drawable.payment_icon)));
+        tbh.addTab(tbh.newTabSpec("Masa").setContent(R.id.tbhMasa).setIndicator("", getDrawable(R.drawable.volume_icon)));
 
         btn=findViewById(R.id.btnConvertir);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                spn = findViewById(R.id.spnDeMonedas);
-                int de = spn.getSelectedItemPosition();
+                try {
+                    int de = 0, a = 0;
+                    temp = (TextView) findViewById(R.id.txtcantidad);
+                    double cantidad = Double.parseDouble(temp.getText().toString());
 
-                spn = findViewById(R.id.spnAMonedas);
-                int a = spn.getSelectedItemPosition();
-
-                temp = (TextView) findViewById(R.id.txtCantidad);
-                double cantidad = Double.parseDouble(temp.getText().toString());
-
-                double respuesta;
-
-                switch(tbh.getCurrentTabTag()) {
-                    case "longitud":
-                        spn = findViewById(R.id.spnDeLongitud);
-                        de = spn.getSelectedItemPosition();
-
-                        spn = findViewById(R.id.spnALongitud);
-                        a = spn.getSelectedItemPosition();
-
-                        cantidad = Double.parseDouble(temp.getText().toString());
-
-                        respuesta = miConversor.convertir(1, de, a, cantidad);
-                        temp = findViewById(R.id.lblRespuesta);
-                        temp.setText("Respuesta: "+ respuesta);
-                        break;
-                    case "monedas":
-                        respuesta = miConversor.convertir(0, de, a, cantidad);
-                        temp = findViewById(R.id.lblRespuesta);
-                        temp.setText("Respuesta: "+ respuesta);
-                        break;
-                    case "masa":
-                        respuesta = miConversor.convertir(2, de, a, cantidad);
-                        temp = findViewById(R.id.lblRespuesta);
-                        temp.setText("Respuesta: "+ respuesta);
-                        break;
+                    int opcion = 0;
+                    switch (tbh.getCurrentTab()) {
+                        case 0:
+                            spnDe = findViewById(R.id.spnDeLongitud);
+                            spnA = findViewById(R.id.spnALongitud);
+                            opcion = 1;
+                            break;
+                        case 1:
+                            spnDe = findViewById(R.id.spnDeAlmacenamiento);
+                            spnA = findViewById(R.id.spnAlmacenamiento);
+                            opcion = 3;
+                            break;
+                        case 2:
+                            spnDe = findViewById(R.id.spnDeMonedas);
+                            spnA = findViewById(R.id.spnAMonedas);
+                            opcion = 0;
+                            break;
+                        case 3:
+                            spnDe = findViewById(R.id.spnDeMasa);
+                            spnA = findViewById(R.id.spnAMasa);
+                            opcion = 2;
+                            break;
+                    }
+                    de = spnDe.getSelectedItemPosition();
+                    a = spnA.getSelectedItemPosition();
+                    temp = findViewById(R.id.lblrespuesta);
+                    temp.setText("Respuesta: " + miConversor.convertir(opcion, de, a, cantidad));
+                }catch (NumberFormatException e){
+                    temp = findViewById(R.id.lblrespuesta);
+                    temp.setText("Por favor ingrese una cantidad a convertir");
+                }catch (Exception e){
+                    temp = findViewById(R.id.lblrespuesta);
+                    temp.setText("ERROR: "+ e.getMessage().toString());
                 }
             }
         });
@@ -75,13 +79,12 @@ public class MainActivity extends AppCompatActivity {
 
 class conversores{
     double[][] valores = {
-            {1, 7.84, 24.66, 36.56, 580.23, 8.75, 0.94, 131.33, 82.54},//moendas
-            {1, 100, 3.28, 39.27, 1.20, 1.09, 0.001, 0.0006}, //Longitudes
-            {}, //Peso
-            {}, //almacenamiento
-            {}, //transferencia de datos (internet)
+            {1, 7.84, 24.63, 36.51, 581.78, 8.75, 0.93, 130.54, 82.52, 0.82},//monedas
+            {1, 100, 3.28084, 39.37008, 1.1963081929167, 1.093613, 0.001, 0.000621},//Longitud
+            {1, 453592.000004704, 453.592, 16, 0.453592, 0.000453592},//Masa
+            {1, 8388608, 1048576,1024, 0.0009765625, 0.00000095367431640625},//Almacenamiento
     };
     public double convertir(int opcion, int de, int a, double cantidad){
-        return valores[opcion][a]/ valores[opcion][de] * cantidad;
+        return valores[opcion][a] / valores[opcion][de] * cantidad;
     }
 }
