5dbea8d877fb910a33100ce37f77b1d26d123108
elise <USIS034221@ugb.edu.sv>
2023-02-19 21:01:59 -0600

    Prueba

--- app/src/main/java/com/ugb/progra2_a1/MainActivity.java
+++ app/src/main/java/com/ugb/progra2_a1/MainActivity.java
@@ -5,58 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TabHost tbh;
     TextView temp;
-    Button btn;
     Spinner spn;
+    Button btn;
+    conversores miConversor= new conversores();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.btnCalcular);
+        tbh = findViewById(R.id.tbhConversores);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("longitud").setContent(R.id.tbLongitud).setIndicator("", getDrawable(R.drawable.longitud)));
+        tbh.addTab(tbh.newTabSpec("monedas").setContent(R.id.tbMonedas).setIndicator("", getDrawable(R.drawable.money)));
+        tbh.addTab(tbh.newTabSpec("masa").setContent(R.id.tbMasa).setIndicator("", getDrawable(R.drawable.masa)));
+
+        btn=findViewById(R.id.btnConvertir);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                temp = (EditText) findViewById(R.id.txtNum1);
-                double num1, num2, resp;
-                num1 = Double.parseDouble(temp.getText().toString());
+                spn = findViewById(R.id.spnDeMonedas);
+                int de = spn.getSelectedItemPosition();
 
-                temp = (EditText) findViewById(R.id.txtNum2);
-                num2 = Double.parseDouble(temp.getText().toString());
+                spn = findViewById(R.id.spnAMonedas);
+                int a = spn.getSelectedItemPosition();
 
-                spn = (Spinner) findViewById(R.id.spnOpciones);
-                String msg = "";
-                switch (spn.getSelectedItemPosition()){
-                    case 0://suma
-                        resp = num1 + num2;
-                        msg = "La suma es: "+ resp;
-                        break;
-                    case 1://REsta
-                        resp = num1 - num2;
-                        msg = "La resta es: "+ resp;
-                        break;
-                    case 2://Multi
-                        resp = num1*num2;
-                        msg="La Multiplicacion es: "+ resp;
-                        break;
-                    case 3://division
-                        resp = num1/num2;
-                        msg = "La division es: "+ resp;
-                        break;
-                    case 4://exponente
-                        resp = Math.pow(num1, num2);
-                        msg = "La exponenciacion es: "+ resp;
-                        break;
-                }
-                temp = (TextView) findViewById(R.id.lblRespuesta);
-                temp.setText(msg);
+                temp = (TextView) findViewById(R.id.txtCantidad);
+                double cantidad = Double.parseDouble(temp.getText().toString());
+
+                double respuesta = miConversor.convertir(0, de, a, cantidad);
+                temp = findViewById(R.id.lblRespuesta);
+                temp.setText("Respuesta: "+ respuesta);
             }
         });
+    }
+}
+
+class conversores{
+    double[][] valores = {
+            {1, 7.84, 24.66, 36.56, 580.23, 8.75, 0.94, 131.33, 82.54},//moendas
+            {}, //Longitudes
+            {}, //Peso
+            {}, //almacenamiento
+            {}, //transferencia de datos (internet)
+    };
+    public double convertir(int opcion, int de, int a, double cantidad){
+        return valores[opcion][a]/ valores[opcion][de] * cantidad;
     }
 }
